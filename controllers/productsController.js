551856d8129b87e0ca0047f3036851b33c7430c3
lindsey tummond <lindseytummond@gmail.com>
2020-07-02 15:08:24 -0700

    updated route for products and render in cards

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,10 +2,12 @@ const db= require("../models")
 
 const products={
     findAll: function(req,res){
+        console.log('all product oute being hit!')
         db.Products
         .find(req.query)
         .then(ProdDB=>{
             console.log(ProdDB)
+            res.json(ProdDB)
         })
         .catch(err=>{
             console.log(err)
