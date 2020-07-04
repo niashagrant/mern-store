b5a9074bbc8c09c8ed0a9852130ccef5c0799d4d
Sara Neves Pereira <59583471+SaraNP-33@users.noreply.github.com>
2020-07-03 17:31:51 -0700

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import Container from 'react-bootstrap/Container';
 
-
-function Login() {
-
-    return (
-        <Container className="col col-sm-1 col-md-9 col-centered">
-            <h6>Log-in Page</h6>
-        </Container>
-    )
-
-=======
 import React from "react";
 import SignIn from "../components/SignIn";
 import Container from "react-bootstrap/Container";
@@ -24,7 +11,6 @@ function Login() {
       </Container>
     </>
   );
->>>>>>> cab0a6d0b81dacd10e3577cd534d00d954e44f04
 }
 
 export default Login;
