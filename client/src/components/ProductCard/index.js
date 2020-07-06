6fd8fe5e14502203f9e6975f94f3011f489828c0
Jennifer Kruk <jenkruk@gmail.com>
2020-07-06 12:23:53 -0700

    added quantity input field to product page

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import "./style.css"
 
 function ProductCard (props) {
+
+    const [quantity, setQuantity] = React.useState(1)
 
     return (
     <Card className="eachProductCard" style={{flex: 1}}>
@@ -19,9 +23,25 @@ function ProductCard (props) {
 
       </Card.Body>
       <Card.Footer className="eachProductFooter">
-      <Card.Text className="float-left text-white">${props.price}</Card.Text>
-          <Button className="btn btn-outline-secondary btn-sm bg-white addToCart float-right">Add to Cart</Button>
-       
+
+
+  <InputGroup className="inputGroup">
+  <Card.Text className="float-left text-white mr-3">${props.price}</Card.Text>
+    <FormControl
+    className="quantityInputField float-right"
+      type="number"
+      min="1"
+      max="9"
+      placeholder="Qty"
+      value={quantity}
+      onChange={event => setQuantity(Number(event.target.value))}
+    />
+
+      <Button className="btn btn-outline-secondary btn-sm bg-white addToCart float-right">
+        Add to Cart
+      </Button>
+
+      </InputGroup>
       </Card.Footer>
     </Card>
 
