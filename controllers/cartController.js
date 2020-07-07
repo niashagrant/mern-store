467c330313d53cd2e56b179982c2122e3fe97f42
Jennifer Kruk <jenkruk@gmail.com>
2020-07-07 10:42:03 -0700

    product adding to db cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,10 +1,18 @@
 const db= require("../models")
 
-// const cart={
-//     update: function(req,res){
-//         update({_id:req.params.id},{$set:{user:req.user._id}},{$set:{product:req.product._id}})
-//     }
-// }
+const cart={
+
+    update: function(req,res){
+        if (req.user) {
+        db.Cart.update({user:req.user._id},{$push:{products: {quantity: req.body.quantity, product: req.body.productid}}}, {upsert: true})
+        .then( () => res.sendStatus(200))
+        .catch( () => res.sendStatus(403))
+        }  else {
+            res.sendStatus(403);
+            console.log("Line 11 of cartController: User is not logged in.");
+        }
+    }
+}
 
 
 
