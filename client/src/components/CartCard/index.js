99d21f4875ac9f32fc5daead18f3685239c75685
Jennifer Kruk <jenkruk@gmail.com>
2020-07-10 12:27:48 -0700

    can now toggle quantity number on cart page

--- client/src/components/CartCard/index.js
+++ client/src/components/CartCard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
@@ -9,6 +9,8 @@ import "./style.css"
 
 
 function CartCard (props) {
+
+  const [quantity, setQuantity] = useState(props.value);
 
     return (
     <Card className="eachProductCard"  key={props.mediaUrl} style={{flex: 1}}>
@@ -29,18 +31,24 @@ function CartCard (props) {
 
       {/* need to make changes below so that user can change/update qty  */}
       <FormControl
-        className="quantityInputField float-right"
+        className="quantityInputField float-right mr-4"
         type="number" 
         min="1"
         max="9"
         placeholder="Qty"
-        value={props.value}
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
         // quantity={props.currentValue}
         />
         {/* <Button  data-user={props.user} data-productid={props.id} onClick={props.handleButton} className="btn btn-outline-secondary btn-sm bg-white addToCart float-right">Add to Cart {props.buttonText}</Button> */}
         <Row className="d-flex justify-content-end w-100">
           {/* To update Qty - maybe add a update button here?  Or can we handle with window location reload?  */}
-          <Button data-id={props.productid} onClick={props.deleteProd} className="btn btn-sm bg-light text-dark delete mt-1">Delete</Button>
+          <Button data-id={props.productid} onClick={props.updateQty} className="btn btn-sm bg-light text-dark delete mt-1 mr-1">
+            Update Qty
+          </Button>
+          <Button data-id={props.productid} onClick={props.deleteProd} className="btn btn-sm bg-light text-dark delete mt-1">
+            Delete
+          </Button>
         </Row>
       </InputGroup>
        
