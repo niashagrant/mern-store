04abf5ef9726a0e43b516fde5e8680e291bc3b12
lindsey tummond <lindseytummond@gmail.com>
2020-07-11 12:55:09 -0700

    big ass arrow product seeds

--- scripts/Seeds.js
+++ scripts/Seeds.js
@@ -36,7 +36,6 @@ const products=[
     "category3": "QWC",
     "description": "Graphic T-Shirt",
     "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest and finished with a banded crew neck.",
-<<<<<<< HEAD
     "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594485953/Screen_Shot_2020-07-11_at_9.37.11_AM_wvvvgd.png"
 },
 {
@@ -49,11 +48,6 @@ const products=[
     "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest and finished with a banded crew neck.",
     "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594485962/Screen_Shot_2020-07-11_at_9.38.49_AM_gn7btv.png"
 },
-=======
-    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594349431/Screen_Shot_2020-07-09_at_7.42.39_PM_wew6k7.png"
-},
-
->>>>>>> 448f1057facd0a292db455ec30eca3587dedeba9
 {
     "name": "QWC Coffee Mug",
     "price": 1800,
@@ -107,6 +101,103 @@ const products=[
     "description": "To hold all your little things",
     "description2": "This zip pouch combines chic French sensibility with no-fuss practicality.",
     "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594485960/Screen_Shot_2020-07-11_at_9.39.44_AM_e8m5ay.png"
+
+},
+{
+    "name": "Big Ass Arrow Men's T-Shirt",
+    "price": 2400,
+    "category1": "clothing",
+    "category2": "men",
+    "category3": "Big Ass Arrow",
+    "description": "Graphic T-Shirt",
+    "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest and finished with a banded crew neck.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497007/Screen_Shot_2020-07-11_at_12.46.11_PM_birhxe.png"
+
+},
+{
+    "name": "Big Ass Arrow Men's Sweatshirt",
+    "price": 4500,
+    "category1": "clothing",
+    "category2": "men",
+    "category3": "Big Ass Arrow",
+    "description": "Sweatshirt",
+    "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497011/Screen_Shot_2020-07-11_at_12.45.01_PM_hzilze.png"
+
+},
+{
+    "name": "Big Ass Arrow Women's T-Shirt",
+    "price": 2400,
+    "category1": "clothing",
+    "category2": "women",
+    "category3": "Big Ass Arrow",
+    "description": "Graphic T-Shirt",
+    "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest and finished with a banded crew neck.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497006/Screen_Shot_2020-07-11_at_12.44.36_PM_ccchox.png"
+},
+{
+    "name": "Big Ass Arrow Women's Tank",
+    "price": 1800,
+    "category1": "clothing",
+    "category2": "women",
+    "category3": "Big Ass Arrow",
+    "description": "Graphic Tank",
+    "description2": "Heavyweight cotton is cut in a standard fit featuring a text graphic across the chest and finished with a banded crew neck.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497010/Screen_Shot_2020-07-11_at_12.46.56_PM_hvfkw6.png"
+},
+{
+    "name": "Big Ass Arrow Coffee Mug",
+    "price": 1800,
+    "category1": "accessories",
+    "category2": "mug",
+    "category3": "Big Ass Arrow",
+    "description": "It could be...",
+    "description2": "This mug brings beauty to the everyday - from baking holiday cookies to traveling the world - with gifts and home goods you will cherish for years to come.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594485954/Screen_Shot_2020-07-11_at_9.41.16_AM_uronry.png"
+
+},
+{ 
+    "name": "Big Ass Arrow Tote",
+    "price": 2500,
+    "category1": "accessories",
+    "category2": "bag",
+    "category3": "QWC",
+    "description": "The bag could be filled with...",
+    "description2": "This tote is sleek, stylish, and classic.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497008/Screen_Shot_2020-07-11_at_12.47.48_PM_qylvlz.png"
+
+},
+{
+    "name": "Big Ass Arrow Spiral Notebook",
+    "price": 900,
+    "category1": "accessories",
+    "category2": "notebook",
+    "category3": "QWC",
+    "description": "Let your dreams run wild",
+    "description2": "A notebook to detail everything from your feelings about a particular situation in your social life to your thoughts on a current event in the political world.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497010/Screen_Shot_2020-07-11_at_12.48.34_PM_varzqo.png"
+
+},
+{
+    "name": "Big Ass Arrow Zip Pouch",
+    "price": 1400,
+    "category1": "accessories",
+    "category2": "zip pouch",
+    "category3": "Big Ass Arrow",
+    "description": "To hold all your little things",
+    "description2": "This zip pouch combines chic French sensibility with no-fuss practicality.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497011/Screen_Shot_2020-07-11_at_12.49.00_PM_n93nri.png"
+
+},
+{
+    "name": "Big Ass Arrow Sticker",
+    "price": 400,
+    "category1": "accessories",
+    "category2": "sticker",
+    "category3": "Big Ass Arrow",
+    "description": "To hold all your little things",
+    "description2": "This zip pouch combines chic French sensibility with no-fuss practicality.",
+    "mediaUrl": "https://res.cloudinary.com/lindseytummond/image/upload/v1594497013/Screen_Shot_2020-07-11_at_12.49.41_PM_dtcrjh.png"
 
 },
 
