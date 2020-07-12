914af9872cc47507e4c4595039316dbf6c014c53
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-07-12 15:32:12 -0700

    troubleshooting...

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 import MenuBar from "./components/Menubar";
 import Marquee from "./components/Marquee";
 import Account from "./pages/Account";
@@ -11,36 +10,43 @@ import SignUp from "./components/SignUp";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 
-
 // const stripePromise = loadStripe("pk_test_51H2jAhF6rrHNM5skrWeDa7Ug2AjxFHAhKeuw8Dv1m2OGNI7WEWf1zebIu8zW5MLhYYygTV7WcfG5L7TOSCtwpfWX00nxZ8LW4t");
 
-function App (props) {
+function App(props) {
+  const [user, setLogin] = useState();
+  console.log(user);
 
-const [user, setLogin]=useState()
- console.log(user);
- 
-
-
-    return (
-      // <Elements stripe={stripePromise}>
-      <Router>
-      <Marquee/>
-      <MenuBar setLogin={setLogin} user={user}{...props}/>
+  return (
+    // <Elements stripe={stripePromise}>
+    <Router>
+      <Marquee />
+      <MenuBar setLogin={setLogin} user={user} {...props} />
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/account" component={Account}/>
-        <Route exact path="/cart" render={(props)=><Cart user={user} {...props}/>}/>
-        <Route exact path="/product/:ProductId" render={(props)=><Product user={user} {...props}/>}/>
-        <Route exact path={"/login"} render={(props)=><SignIn setLogin={setLogin} {...props}/>}/>
-        <Route exact path="/signup" component={()=><SignUp />}/>
-        <Route exact path="/logout"/>
-        <Route exact path="/addToCart"/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/account" component={Account} />
+        <Route
+          exact
+          path="/cart"
+          render={(props) => <Cart user={user} {...props} />}
+        />
+        <Route
+          exact
+          path="/product/:ProductId"
+          render={(props) => <Product user={user} {...props} />}
+        />
+        <Route exact path="/noComment" />
+        <Route
+          exact
+          path={"/login"}
+          render={(props) => <SignIn setLogin={setLogin} {...props} />}
+        />
+        <Route exact path="/signup" component={() => <SignUp />} />
+        <Route exact path="/logout" />
+        <Route exact path="/addToCart" />
       </Switch>
-      </Router>
-      // </Elements>
-
-    );
-  
+    </Router>
+    // </Elements>
+  );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,40 +1,40 @@
-import React, {useState, useEffect} from 'react';
-import Container from 'react-bootstrap/Container';
-import Cards from '../components/Card';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Cards from "../components/Card";
+import axios from "axios";
 
 function Home(props) {
-    let [products, setProducts] = useState([]); //variable ref, variable function
-    useEffect(() => {
-        // console.log('useEffects')
-        axios.get('/allproducts')
-        .then(dbproducts => {
-            setProducts(dbproducts.data)
-            console.log('dbproducts', dbproducts.data)
-        })
-        .catch(error => console.log(error))
-    },[])
+  let [products, setProducts] = useState([]); //variable ref, variable function
+  useEffect(() => {
+    // console.log('useEffects')
+    axios
+      .get("/allproducts")
+      .then((dbproducts) => {
+        setProducts(dbproducts.data);
+        console.log("dbproducts", dbproducts.data);
+        console.log("CATERGORIES", dbproducts.data.catergory3);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    return (
-        
-        <Container className="productContainer col col-sm-1 col-md-9 col-centered pl-4" style={{height: "100vh"}}>
-            {products.map(( element, index ) =>  
-                <Cards 
-                key={element._id}
-                id={element._id} 
-                name={element.name} 
-                image={element.mediaUrl} 
-                price={element.price} 
-                description={element.description}
-                productId={element._id}
-                 /> 
-            )}
-        </Container>
-
-    )
-
+  return (
+    <Container
+      className="productContainer col col-sm-1 col-md-9 col-centered pl-4"
+      style={{ height: "100vh" }}
+    >
+      {products.map((element, index) => (
+        <Cards
+          key={element._id}
+          id={element._id}
+          name={element.name}
+          image={element.mediaUrl}
+          price={element.price}
+          description={element.description}
+          productId={element._id}
+        />
+      ))}
+    </Container>
+  );
 }
-
 
 export default Home;
