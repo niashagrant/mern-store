f5774a5a16e2e2d9cb51c686468b54ace4136ada
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-07-12 19:03:56 -0700

    hard coded badge on cart link.

--- client/src/components/Menubar/index.js
+++ client/src/components/Menubar/index.js
@@ -1,51 +1,74 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import "./style.css"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Badge from "react-bootstrap/Badge";
+import "./style.css";
 import axios from "axios";
 
 function MenuBar(props) {
-  console.log(props)
+  console.log(props);
   let history = useHistory();
-  const {user}=props
-  const logOut=()=>{
-    console.log("logging out")
-    axios.get("/logout")
-    .then(res=>{
-      props.setLogin(null)
-      history.push("/")
-    })
-  }
-  
+  const { user } = props;
+  const logOut = () => {
+    console.log("logging out");
+    axios.get("/logout").then((res) => {
+      props.setLogin(null);
+      history.push("/");
+    });
+  };
 
-return (
-
-<Navbar expand="sm" variant="dark" className="menubar">
-  <Container className="col col-centered">
-  <Navbar.Brand href="/">Queen St. Market</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-    <Nav>
-    
-      <Nav.Link  onClick={()=>{history.push("/")}}>Home</Nav.Link>
-      <Nav.Link  onClick={()=>{history.push("/cart")}}>Cart</Nav.Link>
-      {/* <Nav.Link  onClick={()=>{history.push("/account")}}>Account</Nav.Link> */}
-      {user ? (<>
-      <Nav.Link onClick= {logOut} href="#">Log-Out</Nav.Link>
-      </>):(<>
-      <Nav.Link  onClick={()=>{history.push("/login")}}>Log-In</Nav.Link>
-      {/* <Nav.Link  onClick={()=>{history.push("/signup")}}>Sign-Up</Nav.Link> */}
-     </> )}
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-)
-
+  return (
+    <Navbar expand="sm" variant="dark" className="menubar">
+      <Container className="col col-centered">
+        <Navbar.Brand href="/">Queen St. Market</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link
+              onClick={() => {
+                history.push("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                history.push("/cart");
+              }}
+            >
+              Cart
+              <sup>
+                <Badge variant="light" className="ml-1">
+                  9
+                </Badge>
+              </sup>
+            </Nav.Link>
+            {/* <Nav.Link  onClick={()=>{history.push("/account")}}>Account</Nav.Link> */}
+            {user ? (
+              <>
+                <Nav.Link onClick={logOut} href="#">
+                  Log-Out
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link
+                  onClick={() => {
+                    history.push("/login");
+                  }}
+                >
+                  Log-In
+                </Nav.Link>
+                {/* <Nav.Link  onClick={()=>{history.push("/signup")}}>Sign-Up</Nav.Link> */}
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-
 
 export default MenuBar;
